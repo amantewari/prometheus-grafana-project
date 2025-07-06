01be0fec19ccb11f05ebc728f1da69bc64bbbfe2
aman <at00640834@outlook.com>
2025-07-06 12:25:52 +0000

    Files created

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from prometheus_client import start_http_server, Counter
+
+app = Flask(__name__)
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
+
+@app.route("/")
+def hello():
+    REQUEST_COUNT.inc()
+    return "Hello from monitored app!"
+
+if __name__ == "__main__":
+    start_http_server(5000)
+    app.run(host="0.0.0.0", port=5000)
+
